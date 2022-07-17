ae8506edf30018f24faf167363eab293ca2752e5
Aubry <cobrajayne@gmail.com>
2022-07-17 08:55:28 -0400

    Add API layer

--- src/main/java/com/example/springdemo/alpha/AlphaApplication.java
+++ src/main/java/com/example/springdemo/alpha/AlphaApplication.java
@@ -9,5 +9,4 @@ public class AlphaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlphaApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/springdemo/alpha/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.springdemo.alpha.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21,
+                        "mariam.yohoo@gmail.com"
+                )
+        );
+    }
+
+
+
+}
